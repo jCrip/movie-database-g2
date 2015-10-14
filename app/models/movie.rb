3213d8990b6ebd702748ecf62d1228c4644c0575
Juan Cristobal Pazos <juancri@lalegion.cl>
2015-10-14 19:18:05 -0300

    add validations to movie and reviews model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,7 @@
 class Movie < ActiveRecord::Base
   has_many :reviews, dependent: :destroy
+
+  validates :title, presence: true
+  validates :year, presence: true
+  validates :studio, presence: true
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,5 @@
 class Review < ActiveRecord::Base
   belongs_to :movie
+
+  validates :content, presence: true
 end
