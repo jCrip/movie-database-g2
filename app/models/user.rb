b134bd60a76fa174a042e14a6653efd618b7c6a2
Juan Cristobal Pazos <juancri@lalegion.cl>
2015-10-21 18:37:02 -0300

    add seed of users

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :movies
-  has_many :reviews
+  has_many :movies, dependent: :destroy
+  has_many :reviews, dependent: :destroy
 
   validates :username, uniqueness: { case_sensitive: false }
   validate :validate_username
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,26 +1,31 @@
+User.destroy_all
 Movie.destroy_all
 Review.destroy_all
 Genre.destroy_all
 
-movies = []
+users = []
 
-(1..50).each do |m|
-  movies << Movie.create(
-      title: Faker::Company.name,
-      description: Faker::Lorem.paragraph(3),
-      realease_date: Faker::Date.between(20.years.ago, 1.week.ago),
-      studio: Faker::Name.name
-    )
-end
-
-reviews = []
-
-(1..200).each do |r|
-  reviews << Review.create(
-      content: Faker::Lorem.paragraph(3),
-      movie: movies[rand(movies.length)]
-    )
-end
+users << User.create(
+  name: 'Juan Cristobal',
+  lastname: 'Pazos',
+  username: 'juancri',
+  email: 'admin@imdb.com',
+  password: 'password'
+)
+users << User.create(
+  name: 'Maida',
+  lastname: 'Claro',
+  username: 'mai_clear',
+  email: 'editor@imdb.com',
+  password: 'password'
+)
+users << User.create(
+  name: 'Carlos',
+  lastname: 'Anderson',
+  username: 'neo',
+  email: 'basic@imdb.com',
+  password: 'password'
+)
 
 genres = []
 
@@ -34,3 +39,32 @@ genres << Genre.create(name: 'Thriller')
 genres << Genre.create(name: 'Documental')
 genres << Genre.create(name: 'Musical')
 genres << Genre.create(name: 'Animated')
+
+movies = []
+
+(1..50).each do |m|
+  movies << Movie.create(
+    title: Faker::Company.name,
+    description: Faker::Lorem.paragraph(3),
+    realease_date: Faker::Date.between(20.years.ago, 1.week.ago),
+    studio: Faker::Name.name,
+    user: users[rand(users.length)]
+  )
+end
+
+reviews = []
+
+(1..300).each do |r|
+  reviews << Review.create(
+    content: Faker::Lorem.paragraph(3),
+    movie: movies[rand(movies.length)],
+    user: users[rand(users.length)]
+  )
+end
+
+120.times do
+  MovieGenre.create(
+    movie: movies.sample,
+    genre: genres.sample
+  )
+end
